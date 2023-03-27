ffaafda66b7b3c02e0b69be77b48b2b2d1099afe
Matt <matt8375@yahoo.ca>
2023-03-27 19:25:29 -0400

    Added basic help, but not much functionality yet

--- PNG-view-rem-param.py
+++ PNG-view-rem-param.py
@@ -2,21 +2,61 @@
  
 # add args
 #current version, reads all png in this directory, and dumps in ParametersRemoved
+from dis import show_code
 import os
 from pathlib import Path
 from PIL import Image
+import argparse
 
-inputdir = Path(".")
-outputdir = Path("ParametersRemoved")
+outputdirname="ParametersRemoved"
 
+parser = argparse.ArgumentParser(description ='Basic PNG chunk remover',add_help=True)
+parser.add_argument('--outputdir',default=outputdirname, help ='The output directory. Default is {outputdirname}')
+parser.add_argument('--inputdir',default ='.', help = 'The input directory. Default is cwd')
+parser.add_argument('--force', '-f',action ='store_true',help='Ignore warnings, used when input and output are the same directory')
+parser.add_argument('--parameter',default ='parameters', help = 'The chunk parameter to modify default is "parameters"')
+parser.add_argument('--remove','-r',action = 'store_true', help="remove the pameter specified by 'parameter'")
+parser.add_argument('--add','-a', help = 'The text to add, default')
+parser.add_argument('--show','--display','-s','-d',action ='store_true', help = 'Show/display all text chunks')
+
+
+args =parser.parse_args()
+print(args)
+# print (len(args.add), args.add)
+if args.show:
+    MODE='show'
+elif args.add:
+    MODE='add'
+elif args.remove:
+    MODE='remove'
+else:
+    parser.print_help()
+    print('No action specified, use one of -d -r or -a')
+    quit()
+print ('mode',MODE)
+
+
+inputdir = Path(args.inputdir)
 # Check if input directory exists, otherwise exit
 if not inputdir.exists() or not inputdir.is_dir():
     print("Input directory does not exist")
     exit()
 
+
+#we only need output directories for add and remove
+if not MODE=='show':
+    outputdir = Path(args.outputdir)
+    if inputdir== outputdir:
+        print('Input and output directory are the same')
+        if args.force==False:
+            print('use -f to force')
+            quit()
+
 # Create output directory if it doesn't exist
-if not outputdir.exists() or not outputdir.is_dir():
-    outputdir.mkdir()
+    if not outputdir.exists() or not outputdir.is_dir():
+        outputdir.mkdir()
+
+
 
 # Iterate over all PNG files in the input directory and process them
 for file in inputdir.glob("*.png"):
